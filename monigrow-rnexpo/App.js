8208a9fa2588af981f34737e77992f38399d29ce
Jerry Yan <jeyan@ucsd.edu>
2021-02-20 14:59:43 -0800

    Fix to duplicate firebase error

--- monigrow-rnexpo/App.js
+++ monigrow-rnexpo/App.js
@@ -16,10 +16,19 @@ const firebaseConfig = {
   measurementId: "G-VSW7L2XFSJ"
 };
 
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+   firebase.initializeApp(firebaseConfig);
+}else {
+   firebase.app(); // if already initialized, use that one
+}
 const dbh = firebase.firestore();
 
+function getPlants() {
+  console.log(dbh.collection("plants").get())
+}
+
 export default function App() {
+  getPlants();
   return (
     <View style={styles.container}>
       <Text>Send help</Text>
