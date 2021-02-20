690cf4be212be1a73c7306d2756d8efa216834ec
Jerry Yan <jeyan@ucsd.edu>
2021-02-20 14:11:30 -0800

    Add firebase dependencies

--- monigrow-rnexpo/App.js
+++ monigrow-rnexpo/App.js
@@ -2,10 +2,27 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as firebase from 'firebase'
+import 'firebase/firestore';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA5UXutMWgOAfpzPGipte3NvCh41tsY5k0",
+  authDomain: "monigrow-a9692.firebaseapp.com",
+  projectId: "monigrow-a9692",
+  storageBucket: "monigrow-a9692.appspot.com",
+  messagingSenderId: "959220826867",
+  appId: "1:959220826867:web:ce706c64c76c5b6016ed86",
+  measurementId: "G-VSW7L2XFSJ"
+};
+
+firebase.initializeApp(firebaseConfig);
+const dbh = firebase.firestore();
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Send help</Text>
       <StatusBar style="auto" />
     </View>
   );
