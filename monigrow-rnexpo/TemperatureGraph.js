283409caefd37f291a0aa565ae571fd8486358a8
Jerry Yan <jeyan@ucsd.edu>
2021-02-21 00:09:34 -0800

    Add labels to temperature graph but it doesn't work

--- monigrow-rnexpo/TemperatureGraph.js
+++ monigrow-rnexpo/TemperatureGraph.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { LineChart, Grid } from 'react-native-svg-charts'
+import { View } from 'react-native'
+import { LineChart, Grid, XAxis, YAxis } from 'react-native-svg-charts'
 import scaleTime from 'd3-scale';
 
 import * as firebase from 'firebase'
@@ -37,11 +38,12 @@ export default class TemperatureGraph extends React.Component {
   componentDidMount() {
     db.collection("Temperature").orderBy("time", "desc").limit(this.numFetch)
       .onSnapshot((querySnapshot) => {
-        const snapshotArr = querySnapshot.docs.map( doc => {
+        var snapData = []
+        querySnapshot.docs.forEach( doc => {
           const timeObj = doc.data().time
-          return [timeObj.toDate(), parseFloat(doc.data().temp)]
+          snapData.push({time: timeObj.toDate(), temp: doc.data().temp})
         })
-        this.setState( { recentData: snapshotArr } )
+        this.setState( { recentData: snapData } )
       });
     // this.fetchRecentData()
   }
@@ -53,18 +55,42 @@ export default class TemperatureGraph extends React.Component {
   // fetchRecentData = async () => {}
 
   render() {
+    const contentInset = { top: 20, bottom: 20 }
     return (
-      <LineChart
-        style={{ height: 200 }}
-        data={this.state.recentData}
-        yAccessor={({ item }) => item[1]}
-        xAccessor={({ index }) => this.state.recentData[index][0]}
-        xScale={scaleTime}
-        svg={{ stroke: 'rgb(134, 65, 244)' }}
-        contentInset={{ top: 20, bottom: 20 }}
-      >
-        <Grid />
-      </LineChart>
+      <View style={{ height: 200, padding: 20, flexDirection: 'row' }}>
+        <YAxis
+          style={{ marginHorizontal: 0 }}
+          data={this.state.recentData}
+          yAccessor={({ value }) => value.temp}
+          contentInset={contentInset}
+          svg={{
+              fill: 'grey',
+              fontSize: 10,
+          }}
+          numberOfTicks={10}
+          formatLabel={(value) => `${value.temp}ºF`}
+        />
+        <XAxis
+          style={{ marginHorizontal: -10 }}
+          data={this.state.recentData}
+          xAccessor={({ index }) => this.state.recentData[index].time}
+          scale={scaleTime}
+          formatLabel={((value, index) => value.temp)}
+          contentInset={contentInset}
+          svg={{ fontSize: 10, fill: 'black' }}
+        />
+        <LineChart
+          style={{ height: 200 }}
+          data={this.state.recentData}
+          xAccessor={({ index }) => this.state.recentData[index].time}
+          yAccessor={({ value }) => value.temp}
+          xScale={scaleTime}
+          svg={{ stroke: 'rgb(134, 65, 244)' }}
+          contentInset={contentInset}
+        >
+          <Grid />
+        </LineChart>
+      </View>
     )
   }
 }
