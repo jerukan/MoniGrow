720eeef4b19c9f5821e7424e7726a555a60e1e79
agctute <41280856+agctute@users.noreply.github.com>
2021-02-21 03:08:06 -0800

    creating data (progress)
    crop names , common and botanical sourced from http://www.fao.org/economic/the-statistics-division-ess/world-census-of-agriculture/programme-for-the-world-census-of-agriculture-2000/appendix-3-alphabetical-list-of-crops-botanical-name-and-code-number/en/

--- WebScraper/webscrape.py
+++ WebScraper/webscrape.py
@@ -2,7 +2,27 @@
 # 2/21/21 
 # 2:16AM
 
-from selenium import webdriver
-from selenium.webdriver.common.by import by
-from bs4 import BeautifulSoup
-import pandas as pd
+# from selenium import webdriver
+# from selenium.webdriver.common.by import by
+# from bs4 import BeautifulSoup
+# import pandas as pd
+
+# this section is used to create a file contining a set of botanical crop names
+f = open("WebScraper\cropnames.txt", "r")
+counter = 0
+for line in f:
+    counter+=1
+    if (counter % 6) == 3:
+        print(line)
+    else:
+        continue
+
+f.close()
+
+# class PFAFScrape:
+#     def __init__(self):
+#         self.driver - webdriver.Chrome("C:\Windows\chromedriver.exe")
+
+#     def login(self):
+#         self.driver.get('https://pfaf.org/')
+    
