873f7fe75ccb66851aceb33b6fab9a600109bd6b
agctute <41280856+agctute@users.noreply.github.com>
2021-02-21 03:16:47 -0800

    complete partial data
    complete seperation of data into common and scientific names

--- WebScraper/webscrape.py
+++ WebScraper/webscrape.py
@@ -8,21 +8,46 @@
 # import pandas as pd
 
 # this section is used to create a file contining a set of botanical crop names
-f = open("WebScraper\cropnames.txt", "r")
-counter = 0
-for line in f:
-    counter+=1
-    if (counter % 6) == 3:
-        print(line)
-    else:
-        continue
+# f = open("WebScraper\cropnames.txt", "r")
+# counter = 0
+# bnames = []
+# for line in f:
+#     counter+=1
+#     if (counter % 6) == 3:
+#         print(line)
+#         bnames.append(line)
+#     else:
+#         continue
+# f.close()
 
+# f = open('WebScraper\scientificnames.txt', "w")
+# for name in bnames:
+#     f.write(name)
+# f.close()
+
+
+# #this section is used to create a file containing a set of common crop names
+# f = open("WebScraper\cropnames.txt", "r")
+# counter = 0
+# names = []
+# for line in f:
+#     counter+=1
+#     if (counter % 6) == 1:
+#         print(line)
+#         names.append(line)
+#     else:
+#         continue
+# f.close()
+
+f = open("WebScraper\commonnames.txt", "w")
+for name in names:
+    f.write(name)
 f.close()
 
-# class PFAFScrape:
-#     def __init__(self):
-#         self.driver - webdriver.Chrome("C:\Windows\chromedriver.exe")
+class PFAFScrape:
+    def __init__(self):
+        self.driver - webdriver.Chrome("C:\Windows\chromedriver.exe")
 
-#     def login(self):
-#         self.driver.get('https://pfaf.org/')
+    def login(self):
+        self.driver.get('https://pfaf.org/')
     
