4198b51fc0e108474048b8faa14e90083d18f93e
Jerry Yan <jeyan@ucsd.edu>
2021-02-20 21:48:03 -0800

    Fix component issue

--- monigrow-rnexpo/TemperatureGraph.js
+++ monigrow-rnexpo/TemperatureGraph.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { LineChart, Grid } from 'react-native-svg-charts'
  
-class LineChartExample extends React.PureComponent {
+export default class TemperatureGraph extends React.Component {
     render() {
         const data = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80]
  
